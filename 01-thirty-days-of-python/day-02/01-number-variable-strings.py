ff683076b6154f4e3a263fafde316c325d1c4e6e
Arpit Rathore <arathore@qualys.com>
2020-10-17 17:19:27 +0530

    Added code for day 2

---
+++ 01-thirty-days-of-python/day-02/01-number-variable-strings.py
@@ -0,0 +1,17 @@
+>>> 12 * 31
+
+>>> 12 /31
+
+>>> 5 ** (1/2)
+
+>>> "hello world" * 3
+
+>>> "hello world" + " another"
+
+>>> text = "hello world"
+
+>>>  "hello my name is \"arpit\""
+
+>>> "hello\nworld"
+
+>>> print("hello\nworld")
